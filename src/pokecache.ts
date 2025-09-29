6976b1d6da106aac54f08f37a2bf92c14ad1c9d7
zrose49 <zxr110230@gmail.com>
2025-09-29 16:45:05 -0500

    Added pokecache class logic

---
+++ src/pokecache.ts
@@ -0,0 +1,55 @@
+export type CacheEntry<T> = {
+    createdAt: number;
+    val: T;
+}
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  #interval: number;
+
+  constructor(intervalNum: number) {
+    this.#interval = intervalNum;
+    this.#startReapLoop();
+  }
+
+    add<T>(key: string, value: T) {
+        const entry: CacheEntry<T> = {
+            createdAt: Date.now(),
+            val: value
+        }
+        this.#cache.set(key,entry);
+  }
+
+    get<T>(key: string) {
+    const entry = this.#cache.get(key);
+    if(entry !== undefined) {
+        return entry.val as T;
+    }
+    return undefined;
+  }
+
+  #reap() {
+    const now = Date.now();
+    for(const [key,entry] of this.#cache) {
+        if(now - entry.createdAt > this.#interval) {
+            this.#cache.delete(key);
+        }
+    }
+  }
+
+  #startReapLoop() {
+    this.#reapIntervalId = setInterval(() => {
+      this.#reap();
+    }, this.#interval);
+  }
+
+  stopReapLoop() {
+    if (this.#reapIntervalId) {
+      clearInterval(this.#reapIntervalId);
+      this.#reapIntervalId = undefined;
+    }
+  }
+
+
+}
